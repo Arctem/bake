8d19d7b2c3df01652c80f9b1e996170954e7f616
William Rosenberger <wrosenberger@live.com>
2015-09-20 17:06:35 -0600

    Add file output to the lexer

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,26 +1,62 @@
+#include <iostream>
+#include <string>
+using namespace std;
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 extern int yylineno;
 extern int nelements;
 extern FILE* yyin;
+extern FILE* yyout;
 
 int yylex(void);
+void help(char* cmd_name);
 
 int main(int argc, char** argv)
 {
-  if (argc < 2) {
-    printf("Usage: %s file\n",argv[0]);
-    printf("  where file is a super Cool file to scan\n");
+  string in_fname; // Name of the input file
+  string out_fname; // Name of the output file
+
+  // Get file names
+  char c;
+  while((c = getopt(argc, argv, "i:o:")) != -1) {
+    switch(c) {
+    case 'i': // Input file
+      in_fname = string(optarg);
+      break;
+    case 'o': // Output file
+      out_fname = string(optarg);
+      break;
+    case '?':
+      help(argv[0]);
+      exit(-1);
+      break;
+    }
+  }
+
+  if(in_fname.empty()) {
+    cout << "Must specify input file" << endl;
+    help(argv[0]);
     exit(-1);
   }
 
-  if ((yyin = fopen(argv[1],"r")) == NULL) {
-    printf("Unable to open file %s\n",argv[1]);
-    exit(-1);
+  if(out_fname.empty()) {
+    out_fname = in_fname + "-lex-bake";
+  }
+
+  // Open files
+  yyin = fopen(in_fname.c_str(), "r");
+  yyout = fopen(out_fname.c_str(), "w");
+  if(!yyin or !yyout) {
+    cout << "Error opening files" << endl;
   }
 
   yylex();
 }
 
+void help(char* cmd_name) {
+  cout << "Usage: " << cmd_name << " -i input_file -o output_file\n";
+  cout << "  input_file: File to scan" << endl;
+  cout << "  output_file: File in which to place the output. Defaults to <input_file>-lex-bake" << endl;
+}
