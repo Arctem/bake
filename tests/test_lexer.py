95b1e6c68708b88a00f43452033da8f11aac86a8
William Rosenberger <wrosenberger@live.com>
2015-09-20 20:19:39 -0600

    Added test case to the test script for ensuring that our lexer fails

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -8,6 +8,7 @@ import codecs
 BAKE = "../build/src/bake"
 CL_TEST_DIR = "lexer_tests/" # Directory that contains the .cl files to be tested
 OUTPUT = "lexer_output/" # Where to put the lexer files produced by the official Cool interpreter.
+VERBOSE = "v" in sys.argv
 
 def ref_output(filename):
   """
@@ -18,10 +19,7 @@ def ref_output(filename):
   cmd = "cool --lex {} --out {}".format(CL_TEST_DIR + filename, OUTPUT + filename)
   
   # Run the cool compiler. If it fails, it will return a string containing a description of why.
-  try:
-    fail = check_output(cmd, shell = True)
-  except CalledProcessError:
-    return
+  fail = check_output(cmd, shell = True)
 
   if fail:
     return fail.decode("latin-1")
@@ -38,11 +36,7 @@ def bake_output(filename):
   # Command that runs our lexer on the given file
   cmd = "{} -i {} -o {}".format(BAKE, CL_TEST_DIR + filename, OUTPUT + filename + "-lex-bake")
 
-  try:
-    fail = check_output(cmd, shell = True)
-  except CalledProcessError:
-    return
-
+  fail = check_output(cmd, shell = True)
   if fail:
     return fail.decode("latin-1")
 
@@ -50,25 +44,46 @@ def bake_output(filename):
   with codecs.open(OUTPUT + filename + "-lex-bake", encoding="latin-1") as output:
     return output.read()
 
-def main(args):
+def same_output(filename):
+  """
+    Checks that the reference lexer and Bake have the same output
+  """
+  # Compare output of good test between the bake and reference
+  bake_o = bake_output(filename)
+  ref_o = ref_output(filename)
+
+  if bake_o == ref_o:
+    print("Match!")
+  else:
+    print("Don't match")
+    
+    if VERBOSE:
+      for line in difflib.ndiff(bake_o, ref_o):
+        if line[0] == '+':
+          print("Reference lexer: {}".format(line))
+        elif line[0] == '-':
+          print("Bake: {}".format(line))
+
+def check_failure(filename):
+  """
+    Checks whether the given file fails.
+  """
+  try:
+    bake_output(filename)
+  except CalledProcessError:
+    print("Test faild as excepted")
+
+def main():
   cool_files = [f for f in os.listdir(CL_TEST_DIR) if f.endswith(".cl")] # Get all .cl files in the test directory
 
   for f in cool_files:
     print("----------- Testing {} -----------".format(f))
-    bake_o = bake_output(f)
-    ref_o = ref_output(f)
 
-    if bake_o == ref_o:
-      print("Match!")
+    # Check tests that should error
+    if f in ["invalid-character.cl"]:
+      check_failure(f)
     else:
-      print("Don't match")
-      
-      if "-v" in args:
-        for line in difflib.ndiff(bake_o, ref_o):
-          if line[0] == '+':
-            print("Reference lexer: {}".format(line))
-          elif line[0] == '-':
-            print("Bake: {}".format(line))
+      same_output(f)
 
 if __name__ == "__main__":
-  main(sys.argv)
+  main()
