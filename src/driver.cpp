f249f585274953799fa5be29585dc5ed1a17f95b
Melanie Palmer <mpalmer.design@gmail.com>
2015-10-01 22:14:45 -0600

    Update commandline arguments for this project, closes #12. Add debug prints to BISON, #15.

--- src/driver.cpp
+++ src/driver.cpp
@@ -17,6 +17,7 @@ extern int yylineno;
 extern int nelements;
 extern FILE* yyin;
 extern FILE* yyout;
+extern bool printLex;
 
 int yylex(void);
 int yyparse(void);
@@ -27,6 +28,8 @@ int main(int argc, char** argv)
 {
   string in_fname; // Name of the input file
   string out_fname; // Name of the output file
+  bool run_lex_only;
+
 
   // Check whether only an input file (without the -i option) was given
   if(argc == 2) {
@@ -34,13 +37,18 @@ int main(int argc, char** argv)
   } else {
     // Get file names
     char c;
-    while((c = getopt(argc, argv, "i:o:")) != -1) {
+    while((c = getopt(argc, argv, "i:l:p")) != -1) {
       switch(c) {
       case 'i': // Input file
         in_fname = string(optarg);
         break;
-      case 'o': // Output file
+      case 'l': // output only lex
         out_fname = string(optarg);
+        printLex = true;
+        run_lex_only = true;
+        break;
+      case 'p' :
+        printLex = true;
         break;
       case '?':
         help(argv[0]);
@@ -57,33 +65,61 @@ int main(int argc, char** argv)
     exit(-1);
   }
 
-  // Fail if out is empty
-  if(out_fname.empty()) {
-    out_fname = in_fname + "-lex-bake";
+  if(printLex == true) {
+    // Fail if out is empty
+    if(out_fname.empty()) {
+      out_fname = in_fname + "-lex-bake";
+    }
+
+    yyout = fopen(out_fname.c_str(), "w");
+    if(!yyout) {
+      cout << "Error opening files" << endl;
+    }
+
   }
 
-  // Open files
+  // Open Files
   yyin = fopen(in_fname.c_str(), "r");
-  yyout = fopen(out_fname.c_str(), "w");
-  if(!yyin or !yyout) {
+
+  if(!yyin) {
     cout << "Error opening files" << endl;
   }
 
+  if(run_lex_only == true){
+    // Start the lexer
+    yylex();
+
+    // Close file handlers
+    fclose(yyin);
+    fclose(yyout);
+
+    yylex_destroy();
+    return 0;
+  }
+
   // Start the lexer
-  //yylex();
   yyparse();
 
   // Close file handlers
   fclose(yyin);
-  fclose(yyout);
-  //yylex_destroy();
+
+  if(printLex){
+    fclose(yyout);
+  }
+
+  // Frees memory
+
+  return 0;
 }
+
 
 /* Function prints the help message */
 void help(char* cmd_name) {
   cout << "Usage: " << endl;
   cout << "\t" << cmd_name << " input_file" << endl;
-  cout << "\t" << cmd_name << " -i input_file -o output_file" << endl;
+  cout << "\t" << cmd_name << " -i input_file -l output_file" << endl;
+  cout << "\t" << cmd_name << " -i input_file -y output_file<< endl;
+  cout << "-y: Prints the lexer files" << endl;
   cout << "input_file: File to scan" << endl;
   cout << "output_file: File in which to place the output. Defaults to <input_file>-lex-bake" << endl;
 }
